debeff0417f4c655e9867ba19524a83f89a2b81a
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2020-04-20 14:56:02 -0400

    modified:   journals/tasks.py

--- journals/tasks.py
+++ journals/tasks.py
@@ -52,42 +52,45 @@ def task_db_bibstems_to_master(recs):
 
 @app.task(queue='load-abbrev')
 def task_db_load_abbrevs(recs):
-    if len(recs) > 0:
-        for r in recs:
-            try:
-                session.add(Abbreviations(masterid=r[0],abbreviation=r[1]))
-                session.commit()
-            except:
-                print ("Problem with abbreviation: %s,%s"%(r[0],r[1]))
-                logger.warn("Problem with abbreviation: %s,%s"%(r[0],r[1]))
+    with app.session_scope() as session:
+        if len(recs) > 0:
+            for r in recs:
+                try:
+                    session.add(Abbreviations(masterid=r[0],abbreviation=r[1]))
+                    session.commit()
+                except:
+                    print ("Problem with abbreviation: %s,%s"%(r[0],r[1]))
+                    logger.warn("Problem with abbreviation: %s,%s"%(r[0],r[1]))
 
 
 @app.task(queue='load-issn')
 def task_db_load_issn(recs):
-    if len(recs) > 0:
-        for r in recs:
-            try:
-                session.add(Identifiers(masterid=r[0],id_type='ISSN',id_value=r[1]))
-                session.commit()
-            except Exception as e:
-                print ("Skipping Master ID/ISSN %s,%s"%(r[0],r[1]))
-                logger.warn("Duplicate ISSN ident skipped: %s,%s"%(r[0],r[1]))
-                session.rollback()
-                session.flush()
+    with app.session_scope() as session:
+        if len(recs) > 0:
+            for r in recs:
+                try:
+                    session.add(Identifiers(masterid=r[0],id_type='ISSN',id_value=r[1]))
+                    session.commit()
+                except Exception as e:
+                    print ("Skipping Master ID/ISSN %s,%s"%(r[0],r[1]))
+                    logger.warn("Duplicate ISSN ident skipped: %s,%s"%(r[0],r[1]))
+                    session.rollback()
+                    session.flush()
 
 
 @app.task(queue='load-xref')
 def task_db_load_xref(recs):
-    if len(recs) > 0:
-        for r in recs:
-            try:
-                session.add(Identifiers(masterid=r[0],id_type='CROSSREF',id_value=r[1]))
-                session.commit()
-            except Exception as e:
-                print ("Skipping Master ID/XREF %s,%s"%(r[0],r[1]))
-                logger.warn("Duplicate XREF ident skipped: %s,%s"%(r[0],r[1]))
-                session.rollback()
-                session.flush()
+    with app.session_scope() as session:
+        if len(recs) > 0:
+            for r in recs:
+                try:
+                    session.add(Identifiers(masterid=r[0],id_type='CROSSREF',id_value=r[1]))
+                    session.commit()
+                except Exception as e:
+                    print ("Skipping Master ID/XREF %s,%s"%(r[0],r[1]))
+                    logger.warn("Duplicate XREF ident skipped: %s,%s"%(r[0],r[1]))
+                    session.rollback()
+                    session.flush()
 
 #@app.task(queue='load-csv')
 #def task_db_load_ids(recs):
@@ -96,40 +99,42 @@ def task_db_load_xref(recs):
 @app.task(queue='get-masterid')
 def task_db_get_bibstem_masterid():
     dictionary = {}
-    try:
-        for record in session.query(JournalsMaster.masterid,JournalsMaster.bibstem):
-            dictionary[record.bibstem] = record.masterid
-    except:
-        logger.error("Error: failed to read bibstem-masterid dict from table master")
+    with app.session_scope() as session:
+        try:
+            for record in session.query(JournalsMaster.masterid,JournalsMaster.bibstem):
+                dictionary[record.bibstem] = record.masterid
+        except:
+            logger.error("Error: failed to read bibstem-masterid dict from table master")
     return dictionary
 
 
 @app.task(queue='load-holdings')
 def task_db_load_holdings(recs, infile):
     print "IN TASK task_db_load_holdings"
-    if len(recs) > 0:
-        hold = holdings.Holdings()
+    with app.session_scope() as session:
+        if len(recs) > 0:
+            hold = holdings.Holdings()
 #       for r in recs:
 #           [[bibstem, masterid]] = r.items()
-        output = hold.load_json(infile)
-        h_out = hold.process_output(output)
-        for bibstem, masterid in recs.items():
-            bibstem = str(bibstem)
-            print("Working on bibstem:", bibstem, masterid)
-            try:
-                h_data = h_out[bibstem]
-                for d in h_data:
-                    try:
-                        session.add(Holdings(masterid=masterid, volumes_list=d))
-                        session.commit()
-                    except Exception, err:
-                        print("Skipping Master ID %s" % masterid)
-                        print(err)
-                        session.rollback()
-                        session.commit()
-            except Exception, err:
-                print "Bibstem not found:",bibstem
-                print "Error:",err
-    else:
-        print("You got bupkus.")
+            output = hold.load_json(infile)
+            h_out = hold.process_output(output)
+            for bibstem, masterid in recs.items():
+                bibstem = str(bibstem)
+                print("Working on bibstem:", bibstem, masterid)
+                try:
+                    h_data = h_out[bibstem]
+                    for d in h_data:
+                        try:
+                            session.add(Holdings(masterid=masterid, volumes_list=d))
+                            session.commit()
+                        except Exception, err:
+                            print("Skipping Master ID %s" % masterid)
+                            print(err)
+                            session.rollback()
+                            session.commit()
+                except Exception, err:
+                    print "Bibstem not found:",bibstem
+                    print "Error:",err
+        else:
+            print("You got bupkus.")
     return
